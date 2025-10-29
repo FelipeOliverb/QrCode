5974cab1608282a47bd842c6201de312259a8b26
Felipe de Oliveira <felipe235711@gmail.com>
2025-10-29 15:58:07 -0300

    feito

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import { QRCodeCanvas } from 'qrcode.react';
+import './App.css';
 
 function App() {
+  const [text, setText] = useState(''); 
 
   return (
-    <>
-      <div>
-        
+    <div className="app-container">
+      <div className='titulo'>
+        <h1>Gerador de QR Code</h1>
       </div>
-    
-    </>
-  )
+      
+      
+      <input
+        type="text"
+        placeholder="Digite algo para gerar QR Code"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+
+      <div>
+        {text && <QRCodeCanvas value={text} size={256} />}
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
