ba961b06c11b4a32e4e76151c5dd480e492cca85
Felipe de Oliveira <felipe235711@gmail.com>
2025-10-29 16:31:06 -0300

    resolvendo trens

--- src/App.tsx
+++ src/App.tsx
@@ -27,56 +27,61 @@ function App() {
 
   return (
     <div className="app-container">
-      <h1>Gerador de QR Code Personalizado</h1>
+      <h1 className="titulo">Gerador de QR Code Personalizado</h1>
 
-      <input
-        type="text"
-        placeholder="Digite algo para gerar QR Code"
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-        className="input-text"
-      />
+      <div className="content">
 
-      <div className="logo-upload">
-        <h2>Escolha a imagem para o seu QR Code</h2>
-        <input type="file" accept="image/*" onChange={handleLogoUpload} />
-      </div>
+        <div className="left-column">
+          <input
+            type="text"
+            placeholder="Digite algo para gerar QR Code"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            className="input-text"
+          />
 
-      <div className="preview">
-        {logo && (
-          <div className="logo-preview">
-            <p>Logo selecionada:</p>
-            <img src={logo} alt="Logo" />
+          <div className="logo-upload">
+            <h2>Escolha uma imagem para o seu QR Code</h2>
+            <input type="file" accept="image/*" onChange={handleLogoUpload} />
           </div>
-        )}
-      </div>
 
-      <div className="qr-section">
-        {text && (
-          <>
-            <QRCodeCanvas
-              ref={qrRef}
-              value={text}
-              size={256}
-              bgColor="#000000ff"
-              fgColor="#ffffff"
-              level="H"
-              imageSettings={
-                logo
-                  ? {
-                      src: logo,
-                      height: 50,
-                      width: 50,
-                      excavate: true,
-                    }
-                  : undefined
-              }
-            />
-            {/*<button className="download-btn" onClick={handleDownload}>
-              Baixar QR Code
-            </button>*/}
-          </>
-        )}
+          {logo && (
+            <div className="logo-preview">
+              <p>Logo selecionada:</p>
+              <img src={logo} alt="Logo" />
+            </div>
+          )}
+        </div>
+
+        <div className="right-column">
+          {text ? (
+            <>
+              <QRCodeCanvas
+                ref={qrRef}
+                value={text}
+                size={256}
+                bgColor="#141414"
+                fgColor="#ffffff"
+                level="H"
+                imageSettings={
+                  logo
+                    ? {
+                        src: logo,
+                        height: 50,
+                        width: 50,
+                        excavate: true,
+                      }
+                    : undefined
+                }
+              />
+              <button className="download-btn" onClick={handleDownload}>
+                Baixar QR Code
+              </button>
+            </>
+          ) : (
+            <p className="placeholder-text">Digite algo para gerar o QR Code</p>
+          )}
+        </div>
       </div>
     </div>
   );
